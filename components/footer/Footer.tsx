892e4db75687d664da7daa98d17c9e6f4ebe646c
lucasnull <lucasnull@live.com>
2023-03-19 00:55:35 -0300

    feat: footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,181 +1,127 @@
 import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
-import Text from "$store/components/ui/Text.tsx";
-import Container from "$store/components/ui/Container.tsx";
 
 import Newsletter from "./Newsletter.tsx";
-import type { ComponentChildren } from "preact";
-
-export type IconItem = { icon: AvailableIcons };
-export type StringItem = {
-  label: string;
-  href: string;
-};
-
-export type Item = StringItem | IconItem;
-
-export type Section = {
-  label: string;
-  children: Item[];
-};
-
-const isIcon = (item: Item): item is IconItem =>
-  // deno-lint-ignore no-explicit-any
-  typeof (item as any)?.icon === "string";
-
-function SectionItem({ item }: { item: Item }) {
-  return (
-    <Text variant="caption" tone="default-inverse">
-      {isIcon(item)
-        ? (
-          <div class="border-default border-1 py-1.5 px-2.5">
-            <Icon
-              id={item.icon}
-              width={25}
-              height={20}
-              strokeWidth={0.01}
-            />
-          </div>
-        )
-        : (
-          <a href={item.href}>
-            {item.label}
-          </a>
-        )}
-    </Text>
-  );
-}
-
-function FooterContainer(
-  { children, class: _class = "" }: {
-    class?: string;
-    children: ComponentChildren;
-  },
-) {
-  return <div class={`py-6 px-4 sm:py-12 sm:px-0 ${_class}`}>{children}</div>;
-}
-
 export interface Props {
-  sections?: Section[];
+  institutional: {
+    children: Array<{ label: string; href: string }>;
+  };
+  help: {
+    children: Array<{ label: string; href: string }>;
+  };
+  about: {
+    phone: string;
+    email: string;
+    openingHoursLine1: string;
+    openingHoursLine2: string;
+  };
+  copyright: {
+    line1: string;
+    line2: string;
+    line3: string;
+    line4: string;
+  };
 }
 
-function Footer({ sections = [] }: Props) {
+function Footer({ institutional, help, about, copyright }: Props) {
   return (
-    <footer class="w-full bg-footer flex flex-col divide-y-1 divide-default">
-      <div>
-        <Container class="w-full flex flex-col divide-y-1 divide-default">
-          <FooterContainer>
-            <Newsletter />
-          </FooterContainer>
-
-          <FooterContainer>
-            {/* Desktop view */}
-            <ul class="hidden sm:flex flex-row gap-20">
-              {sections.map((section) => (
-                <li>
-                  <div>
-                    <Text variant="heading-3" tone="default-inverse">
-                      {section.label}
-                    </Text>
-
-                    <ul
-                      class={`flex ${
-                        isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                      } gap-2 pt-2`}
-                    >
-                      {section.children.map((item) => (
-                        <li>
-                          <SectionItem item={item} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* Mobile view */}
-            <ul class="flex flex-col sm:hidden sm:flex-row gap-4">
-              {sections.map((section) => (
-                <li>
-                  <Text variant="body" tone="default-inverse">
-                    <details>
-                      <summary>
-                        {section.label}
-                      </summary>
-
-                      <ul
-                        class={`flex ${
-                          isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                        } gap-2 px-2 pt-2`}
-                      >
-                        {section.children.map((item) => (
-                          <li>
-                            <SectionItem item={item} />
-                          </li>
-                        ))}
-                      </ul>
-                    </details>
-                  </Text>
-                </li>
-              ))}
-            </ul>
-          </FooterContainer>
-        </Container>
+    <footer class="w-full flex flex-col divide-y-1 divide-default">
+      <div class="bg-gray-300 flex flex-col justify-items-center">
+        <Newsletter />
       </div>
 
-      <div>
-        <Container class="w-full">
-          <FooterContainer class="flex justify-between w-full">
-            <Text
-              class="flex items-center gap-1"
-              variant="body"
-              tone="default-inverse"
-            >
-              Powered by{" "}
-              <a
-                href="https://www.deco.cx"
-                aria-label="powered by https://www.deco.cx"
-              >
-                <Icon id="Deco" height={20} width={60} strokeWidth={0.01} />
-              </a>
-            </Text>
-
-            <ul class="flex items-center justify-center gap-2">
-              <li>
-                <a
-                  href="https://www.instagram.com/deco.cx"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Instagram"
-                    strokeWidth={1}
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://www.deco.cx/discord"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Discord logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Discord"
-                    strokeWidth={5}
-                  />
-                </a>
-              </li>
+      <div class="bg-gray-200 flex flex-col items-center justify-center">
+        <div class="flex flex-col lg:flex-row lg:gap-20 gap-0 content-between w-full max-w-[1408px]">
+          <div class="flex flex-col lg:m-8 m-4">
+            <h1 class="text-blue-700 font-extrabold text-sm">INSTITUCIONAL</h1>
+            <ul>
+              {institutional.children.map((item) => (
+                <li key={item.label}>
+                  <a class="text-sm tracking-wider" href={item.href}>
+                    {item.label}
+                  </a>
+                </li>
+              ))}
             </ul>
-          </FooterContainer>
-        </Container>
+          </div>
+
+          <div class="flex flex-col lg:m-8 m-4">
+            <h1 class="text-blue-700 font-extrabold text-sm">AJUDA</h1>
+            <ul>
+              {help.children.map((item) => (
+                <li key={item.label}>
+                  <a class="text-sm tracking-wider" href={item.href}>
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div class="flex flex-col lg:m-8 m-4">
+            <h1 class="text-blue-700 font-extrabold text-sm">
+              SOBRE A LOJA ONLINE
+            </h1>
+            <ul>
+              <li>
+                <p class="font-bold text-sm inline-flex tracking-wider">
+                  Telefone:
+                </p>
+                {about.phone}
+              </li>
+              <li>
+                <p class="font-bold text-sm inline-flex tracking-wider">
+                  Email:
+                </p>
+                {about.email}
+              </li>
+              <br />
+              <li class="font-bold">Horário de Funcionamento</li>
+              <li>{about.openingHoursLine1}</li>
+              <li>{about.openingHoursLine2}</li>
+            </ul>
+          </div>
+
+          <div class="flex flex-col lg:m-3 m-4">
+            <h1 class="text-blue-700 font-extrabold mt-5 text-sm">PAGUE COM</h1>
+            <div class="flex flex-row gap-4">
+              <Icon
+                id={"Elo"}
+                width={30}
+                height={20}
+                strokeWidth={0.6}
+              />
+              <Icon
+                id={"Visa"}
+                width={30}
+                height={20}
+                strokeWidth={0.6}
+              />
+              <Icon
+                id={"Mastercard"}
+                width={30}
+                height={20}
+                strokeWidth={0.6}
+              />
+              <Icon
+                id={"Pix"}
+                width={30}
+                height={20}
+                strokeWidth={0.6}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="bg-gray-100 flex flex-col items-center justify-center w-full p-3">
+          <div class="bg-gray-100 flex flex-col items-center justify-center w-full max-w-[1408px] gap-3">
+            <ul class="text-center text-sm leading-loose">
+              <li>{copyright.line1}</li>
+              <li>{copyright.line2}</li>
+              <li>{copyright.line3}</li>
+              <a href="#">{copyright.line4}</a>
+            </ul>
+          </div>
+        </div>
       </div>
     </footer>
   );
